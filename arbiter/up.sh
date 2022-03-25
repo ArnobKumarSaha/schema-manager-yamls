275c24bd131c1b765c10f891bfc687fc5c758df1
Arnob kumar saha <arnob@appscode.com>
2022-03-25 19:19:29 +0600

    upgrade, verticalScale, Reconfigure done

---
+++ arbiter/up.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [[ "$1" == "clean" ]]; then
+    kubectl delete -f db_shard.yaml
+    kubectl delete -f db_replica.yaml
+    kubectl delete -f tls-replica/issuer.yaml
+    sleep 30
+    kubectl delete ns db
+    sleep 5
+fi 
+
+kubectl create ns db
+kubectl apply -f cm.yaml
+kubectl apply -f tls-replica/secret.yaml
+kubectl apply -f tls-replica/issuer.yaml
+kubectl create secret generic -n db custom-config --from-file=./mongod.conf
+# kubectl apply -f db_shard.yaml
+kubectl apply -f db_replica.yaml
+
+# secret/custom-config                        Opaque                                1      8m25s
+# secret/default-token-b2mk4                  kubernetes.io/service-account-token   3      8m25s
+# secret/mongo-ca                             kubernetes.io/tls                     2      8m25s
+
+# secret/mongodb-auth                         Opaque                                2      8m24s
+# secret/mongodb-client-cert                  kubernetes.io/tls                     3      8m22s
+# secret/mongodb-configsvr-server-cert        kubernetes.io/tls                     3      8m24s
+# secret/mongodb-metrics-exporter-cert        kubernetes.io/tls                     3      8m22s
+# secret/mongodb-mongos-server-cert           kubernetes.io/tls                     3      8m24s
+# secret/mongodb-shard0-arbiter-server-cert   kubernetes.io/tls                     3      8m24s
+# secret/mongodb-shard0-server-cert           kubernetes.io/tls                     3      8m24s
+# secret/mongodb-shard1-arbiter-server-cert   kubernetes.io/tls                     3      8m22s
+# secret/mongodb-shard1-server-cert           kubernetes.io/tls                     3      8m24s
+# secret/mongodb-token-967b4                  kubernetes.io/service-account-token   3      8m24s
+
+
+
+
--- bash/try.sh
+++ bash/try.sh
@@ -2,5 +2,33 @@
 
 # find . -type f ! -iname "*jpg" ! -iname "*.jpeg" ! -iname "*png" -delete
 
-y=$(ls)
-echo "$y"
+retry() {
+    local delay=1
+    local numberOfTry=3
+    local tryNo=1
+    while [[ $tryNo -le $numberOfTry ]] ; do
+        echo "try= $tryNo"
+        str_command="$*"
+        echo "Running command $str_command . . ."
+        out=$("$@")
+        echo "$out"
+
+        if [ "$(echo $out | jq -r '.ok')" == "1" ]; then
+            return 0
+        elif echo $out | jq -r '.errmsg' | grep "HostUnreachable"; then
+            sleep $delay
+        elif echo $out | jq -r '.errmsg' | grep "Host not found"; then
+            sleep $delay
+        elif echo $out | grep "SocketException"; then
+            tryNo=$((tryNo + 1))
+            sleep $delay
+            echo "try inside = $tryNo $numberOfTry"
+        elif [ "$(echo $out | jq -r '.ok')" == "0" ]; then
+            exit 1  # kill the container
+        else
+            return 0
+        fi
+    done
+}
+
+retry echo SocketException
